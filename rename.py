1f0fe395bfc8fb056baff52e6ad912e05e4198eb
MCTF-Alpha-27 <85936935+MCTF-Alpha-27@users.noreply.github.com>
2022-03-11 17:27:57 +0800

    新鲜出炉
    此插件可以更改显示的ID

---
+++ rename.py
@@ -0,0 +1,33 @@
+"""
+此插件可以更改显示的ID
+"""
+from .lib.root import *
+
+__name__ = "多姿多彩的ID"
+__version__ = "1.1"
+__author__ = "Jerry"
+
+def rename():
+    rename = tk.Tk()
+    rename.title('更改ID')
+    rename.minsize(250,100)
+
+    rename_input = ttk.Entry(rename)
+    
+    def ID_change():
+        nonlocal rename_input
+        with open("name.ini", "w") as f:
+            f.write(rename_input.get())
+            
+        msg.showinfo('更改ID','成功更改ID为%s'%(' "' + rename_input.get() + '"'))
+
+    rename_ID = ttk.Button(rename, text = '更改ID', command = ID_change)
+    quit_ = ttk.Button(rename, text = "完成", command = rename.destroy)
+    
+    rename_input.pack()
+    rename_ID.pack()
+    quit_.pack()
+
+    rename.mainloop()
+
+config_menu.add_command(label = "更改ID", command = rename)
